0cf1f5a51d6b848224d2ebe872b2e66d3613c6f6
Mattie <mattbware3@gmail.com>
2021-02-26 11:08:10 +0000

    Way better labelled checkbox

--- ProceduralDungeonTest/UI/LabelledCheckbox.cs
+++ ProceduralDungeonTest/UI/LabelledCheckbox.cs
@@ -3,56 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace PDT.UI
 {
-    // TODO refactor this as inheriting horizontal layout
-    public class LabelledCheckbox : UIElement
+    public class LabelledCheckbox : HorizontalLayout
     {
-        private HorizontalLayout layout;
-        private Checkbox         checkbox;
-        private Label            label;
+        private Checkbox checkbox;
+        private Label    label;
 
         public bool Selected
         {
             get { return checkbox.Selected; }
-            set { SetSelected(value); }
+            set { checkbox.Selected = value; }
         }
-
-        public new Vector2 Size
+        
+        public LabelledCheckbox(Point pos, Vector2 checkboxSize, string text, SpriteFont font, Color bgColor, Color bgHoveredColor, Color selectColor, Color textColor, int spacing = 8)
+            : base(pos, spacing)
         {
-            get { return layout.Size; }
-            set { layout.Size = value; }
-        }
-
-        public new Point Position
-        {
-            get { return layout.Position; }
-            set { layout.Position = value; }
-        }
-
-        public LabelledCheckbox(Point pos, Vector2 size, string text, SpriteFont font, Color bgColor, Color bgHoveredColor, Color selectColor, Color textColor)
-            : base (pos, size)
-        {
-            layout = new HorizontalLayout(pos, 8);
-            checkbox = new Checkbox(Point.Zero, size, bgColor, bgHoveredColor, selectColor);
-            layout.AddElement(checkbox);
+            checkbox = new Checkbox(Point.Zero, checkboxSize, bgColor, bgHoveredColor, selectColor);
             label = new Label(text, font, Point.Zero, textColor);
-            layout.AddElement(label);
-        }
-
-        public void SetSelected(bool value)
-        {
-            checkbox.SetSelected(value);
-        }
-
-        public override void Update()
-        {
-            base.Update();
-            layout.Update();
-        }
-
-        public override void Render(SpriteBatch sb)
-        {
-            base.Render(sb);
-            layout.Render(sb);
+            AddElement(checkbox);
+            AddElement(label);
         }
     }
 }
